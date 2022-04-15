4d18aad529945ed8732a7d0b09169fd44f9be9f7
José <jose.santos@conceptu.ind.br>
2022-04-15 12:49:04 -0300

    Adicionando testes no CI

---
+++ my_file_explorer/user/tests/test_user_api.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+CREATE_USER_URL = reverse('register')
+LOGIN_URL = reverse('login')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the user API (public) """
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'username': 'jvss',
+            'first_name': 'jose',
+            'password': 'test123'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
