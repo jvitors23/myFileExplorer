bb0b7e16b1f08e1e6e440a5555309857fa05ff8d
José <jose.santos@conceptu.ind.br>
2022-04-16 11:20:38 -0300

    Creating File Model

--- my_file_explorer/explorer/models.py
+++ my_file_explorer/explorer/models.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.db import models
 
@@ -25,3 +28,27 @@ class Folder(BaseModel):
 
     def get_children_folders(self):
         return Folder.objects.filter(parent_folder=self)
+
+    def get_children_files(self):
+        return File.objects.filter(parent_folder=self)
+
+
+def get_s3_file_path(instance, filename):
+    user = instance.owner.username
+    parent_folder = instance.parent_folder.name
+    name, extension = os.path.splitext(filename)
+    return f'{user}/{parent_folder}/{name}_{uuid.uuid4()}{extension}'
+
+
+class File(BaseModel):
+
+    name = models.CharField(max_length=30)
+    parent_folder = models.ForeignKey('explorer.Folder', null=False,
+                                      on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE)
+    file = models.FileField(upload_to=get_s3_file_path)
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ('name', 'parent_folder', 'owner')
