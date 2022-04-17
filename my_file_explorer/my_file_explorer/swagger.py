02e4509a49921e0c056c7785cf2c510a7ff6ca3e
José <jose.santos@conceptu.ind.br>
2022-04-17 03:34:16 -0300

    Enable https scheme swagger

--- my_file_explorer/my_file_explorer/swagger.py
+++ my_file_explorer/my_file_explorer/swagger.py
@@ -1,6 +1,15 @@
+from drf_yasg.generators import OpenAPISchemaGenerator
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+   def get_schema(self, request=None, public=False):
+      schema = super().get_schema(request, public)
+      schema.schemes = ["http", "https"]
+      return schema
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -12,4 +21,5 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
+   generator_class=BothHttpAndHttpsSchemaGenerator,
 )
