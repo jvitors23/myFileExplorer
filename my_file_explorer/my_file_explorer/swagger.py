03b71d777690b57901e2d6ef842906c207232bce
José <jose.santos@conceptu.ind.br>
2022-04-18 17:01:22 -0300

    Dinamic changing swagger scheme

--- my_file_explorer/my_file_explorer/swagger.py
+++ my_file_explorer/my_file_explorer/swagger.py
@@ -2,12 +2,16 @@ from drf_yasg.generators import OpenAPISchemaGenerator
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
 
 
 class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
-        schema.schemes = ["http", "https"]
+        if settings.DEBUG:
+            schema.schemes = ["http"]
+        else:
+            schema.schemes = ["https"]
         return schema
 
 
