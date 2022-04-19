5baee8b42ba74881df218032aebeb07b63047305
José Vitor Santos Silva <52494917+JvitorS23@users.noreply.github.com>
2022-04-18 22:18:41 -0300

    Frontend (#3)
    * Add frontend

--- my_file_explorer/user/urls.py
+++ my_file_explorer/user/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('token/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('register/', views.RegisterUserView.as_view(), name='register'),
+    path('signup/', views.SignUpUserView.as_view(), name='signup'),
     path('me/', views.ManageUserView.as_view(), name='profile'),
 ]
